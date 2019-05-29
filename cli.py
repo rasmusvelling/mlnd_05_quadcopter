dbe00e34d21bb39f17dbc0216613a86a81c8209e
rasmusvelling <rasmusveling>
2019-05-29 22:18:32 +0200

    added .idea to gitignore

--- cli.py
+++ cli.py
@@ -5,11 +5,14 @@ from task import Task
 from agents.ddpg import Agent
 
 import matplotlib.pyplot as plt
+plt.interactive(False)
 from mpl_toolkits.mplot3d import Axes3D
 
 #######################
+# Helper functions
+
 # 3d print by https://github.com/mohamedabdallah1996
-def quadcopter_3d_plot(results, vars=['x', 'y', 'z'], title=''):
+def quadcopter_3d_plot(results, vars=['x', 'y', 'z'], title='', save_to=None):
     x = results[vars[0]]
     y = results[vars[1]]
     z = results[vars[2]]
@@ -20,62 +23,98 @@ def quadcopter_3d_plot(results, vars=['x', 'y', 'z'], title=''):
     cax = ax.scatter(x, y, z, c=c, cmap='YlGn')
     ax.set(xlabel=vars[0], ylabel=vars[1], zlabel=vars[2], title=title)
     fig.colorbar(cax, label='Time step (s)', pad=0.1, aspect=40)
-    plt.show();
 
+    if save_to is None:
+        plt.show()
+    else:
+        plt.savefig(save_to)
+
+
+def make_df_rewards(all_results):
+    rewards_summed = {}
+    for key, value in all_results.items():
+        rewards_summed[key] = [sum(value['reward'])]
+    df = pd.DataFrame(rewards_summed, index=['Reward']).transpose()
+    return df
+
+
+def plot_rewards(all_results, save_to=None):
+    df = make_df_rewards(all_results)
+    fig = plt.figure(figsize=(8, 4), dpi=100)
+    plt.plot(df['Reward'])
+    if save_to is None:
+        plt.show()
+    else:
+        plt.savefig(save_to)
 
 
 ########################
-num_episodes = 10
-runtime = 50.
+# Main function
+def do_learning(
+    num_episodes=1000,
+    runtime=5.,
+    # we want the quad copter to go high up!
+    target_pos=np.array([0., 0., 75.]),
+    init_pos=np.array([0., 0., 10., 0., 0., 0.]),
+):
 
-# we want the quad copter to go high up!
-target_pos = np.array([0., 0., 100.])
+    task = Task(
+        target_pos=target_pos,
+        init_pose=init_pos,
+        runtime=runtime
+    )
+    agent = Agent(
+        task=task,
+        exploration_mu=0, exploration_theta=0.15, exploration_sigma=0.2, tau=0.01
+    )
 
-init_pos = np.array([0., 0., 10., 0., 0., 0.])
-task = Task(
-    target_pos=target_pos,
-    init_pose=init_pos,
-    runtime=runtime
-)
-agent = Agent(
-    task=task,
-    exploration_mu=0, exploration_theta=0.15, exploration_sigma=0.2, tau=0.01
-)
+    labels = [
+        'time', 'x', 'y', 'z', 'phi', 'theta', 'psi', 'x_velocity', 'y_velocity', 'z_velocity', 'phi_velocity',
+        'theta_velocity', 'psi_velocity', 'rotor_speed1', 'rotor_speed2', 'rotor_speed3', 'rotor_speed4', 'reward']
 
-labels = ['time', 'x', 'y', 'z', 'phi', 'theta', 'psi', 'x_velocity',
-          'y_velocity', 'z_velocity', 'phi_velocity', 'theta_velocity',
-          'psi_velocity', 'rotor_speed1', 'rotor_speed2', 'rotor_speed3', 'rotor_speed4']
+    all_results = {}
 
-all_results = {}
+    for i_episode in range(1, num_episodes+1):
+        state = agent.reset_episode() # start a new episode
+        results = {x: [] for x in labels}
+        while True:
+            # agent chooses action
+            action = agent.act(state)
+            # Trying to force no action does make quad copter just drop to the floor
+            # action = [0,0,0,0]
 
-for i_episode in range(1, num_episodes+1):
-    state = agent.reset_episode() # start a new episode
-    results = {x: [] for x in labels}
-    while True:
-        # agent chooses action
-        action = agent.act(state)
-        # Trying to force no action does make quad copter just drop to the floor
-        # action = [0,0,0,0]
+            # Trying to force action full throttle upwards
+            #action = [9000, 9000, 9000, 9000]
 
-        # Feed action to environment and get next state
-        next_state, reward, done = task.step(action)
+            # Feed action to environment and get next state
+            next_state, reward, done = task.step(action)
 
-        # Make agent learn from step
-        agent.step(action, reward, next_state, done)
+            # Make agent learn from step
+            agent.step(action, reward, next_state, done)
 
-        # IMPORTANT - update state for next iteration
-        state = next_state
+            # IMPORTANT - update state for next iteration
+            state = next_state
 
-        # save results
-        to_write = [task.sim.time] + list(task.sim.pose) + list(task.sim.v) + list(task.sim.angular_v) + list(action)
-        for ii in range(len(labels)):
-            results[labels[ii]].append(to_write[ii])
+            # save results
+            to_write = [task.sim.time] + list(task.sim.pose) + list(task.sim.v) + list(task.sim.angular_v) + list(action) + [reward]
+            for ii in range(len(labels)):
+                results[labels[ii]].append(to_write[ii])
 
-        if done:
-            if i_episode % 50 == 0:
-                print("Done:  " + str(i_episode))
-            break
-    all_results[i_episode] = results
-    sys.stdout.flush()
+            if done:
+                if i_episode % 100 == 0:
+                    print("Done:  " + str(i_episode))
+                    plot_rewards(all_results, save_to="plots\\plot_"+str(i_episode)+".png")
+                break
+        all_results[i_episode] = results
+        sys.stdout.flush()
+
+    return all_results
+
+all_results = do_learning(num_episodes=1000)
+
+plot_rewards(all_results)
+
+df_rew = make_df_rewards(all_results)
 
 quadcopter_3d_plot(all_results[len(all_results)], vars=['x', 'y', 'z'], title='')
+quadcopter_3d_plot(all_results[8])
